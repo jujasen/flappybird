5782f20c94894aea697015b415d00762516c7c28
Julz <jujasen@hotmail.com>
2021-02-01 20:30:48 +0100

    added top pipe and bird graphic

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let birdBottom = 100;
     let gravity = 2;
     let gameDone = false;
+    let gap = 450;
 
     function startGame() {
         if(birdBottom > 0) {
@@ -26,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function jump() {
         if(birdBottom < 400) {
-            birdBottom += 100;
+            birdBottom += 50;
             bird.style.bottom = birdBottom + 'px';
             console.log(birdBottom)
         }
@@ -36,28 +37,36 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('keypress', control);
 
     function createPipes () {
-        let randomHeight = Math.random() * 200;
         let pipeLeft = 500;
+        let randomHeight = Math.random() * 100;
         let pipeBottom = randomHeight;
         const pipe = document.createElement('div');
+        const pipeTop = document.createElement('div');
         if (!gameDone) {
+            pipeTop.classList.add('pipe-top');
             pipe.classList.add('pipe');
         }
         game.appendChild(pipe);
+        game.appendChild(pipeTop);
         pipe.style.left = pipeLeft + 'px';
+        pipeTop.style.left = pipeLeft + 'px';
         pipe.style.bottom = pipeBottom + 'px';
+        pipeTop.style.bottom = pipeBottom + gap + 'px';
 
         function movePipe() {
             pipeLeft -= 2;
             pipe.style.left = pipeLeft + 'px';
+            pipeTop.style.left = pipeLeft + 'px';
+
 
 
             if (pipeLeft === -60) {
                 clearInterval(pipeTimer);
                 game.removeChild(pipe);
+                game.removeChild(pipeTop);
             }
 
-            if (pipeLeft > 200 && pipeLeft < 280 && birdLeft === 220 && birdBottom < pipeBottom + 100|| birdBottom === 0) {
+            if (pipeLeft > 200 && pipeLeft < 280 && birdLeft === 220 && (birdBottom < pipeBottom + 100 || birdBottom > pipeBottom + gap - 250.5) || birdBottom === 0) {
                 console.log("game over");
                 gameOver ();
                 clearInterval(pipeTimer);
